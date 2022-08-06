c90730ef14209a56a660ce982ce53a7a5b7d1671
ldgfelipe <ldgfelipecarre@gmail.com>
2022-08-05 22:48:29 -0500

    <update> crea ssr firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,17 @@
+import * as functions from "firebase-functions";
+
+const { Nuxt } = require("nuxt-start");
+
+const nuxtConfig = require("../nuxt.config.js");
+
+const config = {
+  ...nuxtConfig,
+  dev: false,
+  buildDir: "nuxt",
+};
+const nuxt = new Nuxt(config);
+
+exports.ssrapp = functions.https.onRequest(async (req, res) => {
+  await nuxt.ready();
+  nuxt.render(req, res);
+});
---
+++ nuxt.config.js
@@ -0,0 +1,109 @@
+module.exports={
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - planetapreescolar-ssr',
+    title: 'planetapreescolar-ssr',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyB8GyvM9PdvjufoAyEZv2uRQLVhVQM-Z-o",
+          authDomain: "educadorafirebase.firebaseapp.com",
+          databaseURL: "https://educadorafirebase.firebaseio.com",
+          projectId: "educadorafirebase",
+          storageBucket: "educadorafirebase.appspot.com",
+          messagingSenderId: "4922270561",
+          appId: "1:4922270561:web:d12332e6277b143e5c9b20",
+          measurementId: "G-E7Y6JTQG51"
+        },
+        services: {
+          auth: true // Just as example. Can be any other service.
+        }
+      }
+    ]
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: '#ad227d',
+          melon:'#ff6c5c',
+          verylight:'#ebe4eb',
+          secondary: '#c93b98',
+          greysh:'#484848',
+          morado:'#3d1e55',
+          accent: '#82B1FF',
+          butoncolor:'#135568',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+          prysec: '#6C0040'
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    buildModules: [
+      '@nuxt/typescript-build',
+      '@nuxtjs/vuetify',
+      '@nuxtjs/firebase'
+    ],
+
+  }
+}
